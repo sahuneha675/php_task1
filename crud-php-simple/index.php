660f221605db7c9ec5c61f8c5795b6797feb62a2
sahuneha675 <sahuneha675@github.com>
2023-03-17 22:45:52 +0530

    search functionality and css changes

--- crud-php-simple/index.php
+++ crud-php-simple/index.php
@@ -11,6 +11,26 @@ $productresult = mysqli_query($mysqli, "SELECT p.pid,p.product_name,p.descriptio
 FROM `products` p 
 LEFT JOIN vendor v ON v.vid=p.vendor_id 
 LEFT JOIN employees emp ON emp.id=p.added_by");
+
+
+// Retrieve search term from form or URL
+$search_term = $_GET['search'];
+
+// Construct SQL query
+$sql = "SELECT * FROM employees WHERE name LIKE '%$search_term%'";
+
+// Execute query
+$searchresult = mysqli_query($mysqli, $sql);
+
+// Display results
+// if (mysqli_num_rows($searchresult) > 0) {
+//     while ($row = mysqli_fetch_assoc($searchresult)) {
+//         echo $row['name'] . "<br>";
+//     }
+// } else {
+//     echo "No results found.";
+// }
+
 ?>
 
 <html>
@@ -31,8 +51,10 @@ LEFT JOIN employees emp ON emp.id=p.added_by");
 		<a href="addvendor.php">Add Vender</a>
 	</p>
 	<p>
-	<input type="text" placeholder="Search.." name="search">
-	<button>Search</button>
+	<form action="index.php" method="GET">
+		<input id="search" name="search" type="text" placeholder="Type here">
+		<input id="submit" type="submit" value="Search">
+	</form>
     </p>
 	<h4>Employees</h4>
 	<table width='80%' border=0>
@@ -48,18 +70,42 @@ LEFT JOIN employees emp ON emp.id=p.added_by");
 		</tr>
 		<?php
 		// Fetch the next row of a result set as an associative array
-		while ($res = mysqli_fetch_assoc($result)) {
-			echo "<tr>";
-			echo "<td>".$res['email']."</td>";	
-			echo "<td>".$res['password']."</td>";
-			echo "<td>".$res['name']."</td>";
-			echo "<td>".$res['role']."</td>";
-			echo "<td>".$res['gender']."</td>";
-			echo "<td>".$res['salary']."</td>";
-			echo "<td>".$res['address']."</td>";
-			echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
-			<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+
+		if(!empty($searchresult)){
+			while ($res = mysqli_fetch_assoc($searchresult)) {
+				if($res['gender'] == 'M'){
+					//blue
+					$gender = "<td style='color:blue'>".$res['gender']."</td>";
+				}else{
+					//red
+					$gender = "<td style='color:red'>".$res['gender']."</td>";
+				}
+				echo "<tr>";
+				echo "<td>".$res['email']."</td>";	
+				echo "<td>".$res['password']."</td>";
+				echo "<td>".$res['name']."</td>";
+				echo "<td>".$res['role']."</td>";
+				echo $gender;
+				echo "<td>".$res['salary']."</td>";
+				echo "<td>".$res['address']."</td>";
+				echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
+				<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+			}
+		}else{
+			while ($res = mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+				echo "<td>".$res['email']."</td>";	
+				echo "<td>".$res['password']."</td>";
+				echo "<td>".$res['name']."</td>";
+				echo "<td>".$res['role']."</td>";
+				echo "<td>".$res['gender']."</td>";
+				echo "<td>".$res['salary']."</td>";
+				echo "<td>".$res['address']."</td>";
+				echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | 
+				<a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+			}
 		}
+			
 		?>
 	</table>
 
